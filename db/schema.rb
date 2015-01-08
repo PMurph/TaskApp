d77ae2fbd3a24d06f5e3cb40b7097a6f3ba5e8a3
Patrick Murphy <murph.pm@gmail.com>
2015-01-08 07:25:13 -0600

    Added migration for adding verfied column to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150101023448) do
+ActiveRecord::Schema.define(version: 20150108131827) do
 
   create_table "users", force: true do |t|
     t.string   "username"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150101023448) do
     t.string   "email_address"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "verified",      default: false
   end
 
 end
